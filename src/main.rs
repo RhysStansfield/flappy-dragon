a31d67694162ccd8c27fd29d372a5ed49f387901
Rhys Stansfield <rhys.stansfield@proton.me>
2024-08-26 13:47:55 +0100

    Implement flapping dragon sprite

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,41 @@ enum GameMode {
 
 const SCREEN_WIDTH : i32 = 80;
 const SCREEN_HEIGHT : i32 = 50;
-const FRAME_DURATION : f32 = 75.0;
+const FRAME_DURATION : f32 = 50.0;
+const SPRITE_HEIGHT : i32 = 8;
+const SPRITE_WIDTH : i32 = 8;
 
 struct Player {
     x: i32,
-    y: i32,
+    y: f32,
     velocity: f32,
+    flap_cycle: f32,
 }
 
 impl Player {
-    fn new(x: i32, y: i32) -> Self {
+    fn new(x: i32, y: f32) -> Self {
         Player {
             x,
             y,
             velocity: 0.0,
+            flap_cycle: 0.0,
         }
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(
-            0,
-            self.y,
-            YELLOW,
-            BLACK,
-            to_cp437('@')
+        ctx.set_active_console(1);
+        ctx.cls();
+
+        let render_y = self.y as i32 - (SPRITE_HEIGHT / 2);
+
+        ctx.add_sprite(
+            Rect::with_size(0, render_y, SPRITE_WIDTH, SPRITE_HEIGHT),
+            10,
+            RGBA::from_f32(1.0, 1.0, 1.0, 1.0),
+            self.flap_cycle as usize,
         );
+
+        ctx.set_active_console(0);
     }
 
     fn gravity_and_move(&mut self) {
@@ -40,15 +50,28 @@ impl Player {
             self.velocity += 0.2;
         }
 
-        self.y += self.velocity as i32;
+        self.y += self.velocity;
         self.x += 1;
-        if self.y < 0 {
-            self.y = 0;
+        if self.y < 0.0 {
+            self.y = 0.0;
+        }
+
+        if self.flap_cycle > 3.0 {
+            self.flap_cycle -= 0.4;
+        } else if self.flap_cycle > 2.0 {
+            self.flap_cycle -= 0.1;
+        } else if self.flap_cycle > 0.0 {
+            self.flap_cycle -= 0.5;
+        }
+
+        if self.flap_cycle < 0.0 {
+            self.flap_cycle = 0.0
         }
     }
 
     fn flap(&mut self) {
         self.velocity = -2.0;
+        self.flap_cycle = 3.9;
     }
 }
 
@@ -64,7 +87,7 @@ impl Obstacle {
         Obstacle {
             x,
             gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - score)
+            size: i32::max(8, 20 - score)
         }
     }
 
@@ -95,9 +118,9 @@ impl Obstacle {
 
     fn hit_obstacle(&self, player: &Player) -> bool {
         let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
-        let player_above_gap = player.y < self.gap_y - half_size;
-        let player_below_gap = player.y > self.gap_y + half_size;
+        let does_x_match = player.x < self.x && player.x + SPRITE_WIDTH >= self.x;
+        let player_above_gap = (player.y as i32) < self.gap_y - half_size;
+        let player_below_gap = (player.y as i32) > self.gap_y + half_size;
 
         does_x_match && (player_above_gap || player_below_gap)
     }
@@ -114,7 +137,7 @@ struct State {
 impl State {
     fn new() -> Self {
         Self {
-            player: Player::new(5, 25),
+            player: Player::new(5, 25.0),
             frame_time: 0.0,
             obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
@@ -123,7 +146,7 @@ impl State {
     }
 
     fn restart(&mut self) {
-        self.player = Player::new(5, 25);
+        self.player = Player::new(5, 25.0);
         self.frame_time = 0.0;
         self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.score = 0;
@@ -156,13 +179,16 @@ impl State {
             self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
-        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+        if self.player.y as i32 > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        ctx.set_active_console(0);
         ctx.print_centered(5, "Welcome to Flappy Dragon");
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
@@ -178,6 +204,9 @@ impl State {
 
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        ctx.set_active_console(0);
         ctx.print_centered(5, "You are dead!");
         ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
@@ -203,9 +232,22 @@ impl GameState for State {
     }
 }
 
+embedded_resource!(DRAGON, "../resources/dragon-frames-sprite.png");
+
 fn main() -> BError {
+    link_resource!(DRAGON, "resources/dragon-frames-sprite.png");
+
     let context = BTermBuilder::simple80x50()
+        .with_sprite_console(80, 50, 0)
+        .with_font("terminal8x8.png", 8, 8)
         .with_title("Flappy Dragon")
+        .with_sprite_sheet(
+            SpriteSheet::new("resources/dragon-frames-sprite.png")
+                .add_sprite(Rect::with_size(0, 0, 235, 170))
+                .add_sprite(Rect::with_size(235, 0, 235, 170))
+                .add_sprite(Rect::with_size(470, 0, 235, 170))
+                .add_sprite(Rect::with_size(705, 0, 235, 170)),
+        )
         .build()?;
 
     main_loop(context, State::new())
